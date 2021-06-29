ac77fa30cb39fbffc955cb4e19043e815b4bffac
Muhaimin CS <muhaimincs@gmail.com>
2021-06-29 12:02:23 +0800

    add margin

--- pages/index.js
+++ pages/index.js
@@ -124,7 +124,7 @@ const blog = () => {
       </div>
       
       <img src="/img/chart.svg" />
-      <div className="bg-gray-50 dark:bg-gray-800 border-t border-b border-gray-50">
+      <div className="bg-gray-50 dark:bg-gray-800 border-t border-b border-gray-50 mt-3">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <div>
             <span className="block text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl dark:text-white">Ongoing WordPress Development and Growth</span>
