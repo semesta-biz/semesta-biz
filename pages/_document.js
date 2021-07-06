203fc55e5dd6a6b7f27c0928df1a3dca2b37588d
Muhaimin CS <muhaimincs@gmail.com>
2021-07-07 07:38:01 +0800

    switch to html

--- pages/_document.js
+++ pages/_document.js
@@ -136,9 +136,9 @@ class MyDocument extends Document {
               />
             </>
           )}
-          <Script strategy="lazyOnload">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T5BBMSN');`}
-          </Script>
+          <Script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T5BBMSN');`}} 
+          />
         </Head>
         <body className="bg-day dark:bg-night">
         <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5BBMSN" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
